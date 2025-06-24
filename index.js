ae1fcb58f59a855c1778fe78d7a1e89f16a7d022
Prasad Kute <prasadkute0708@gmail.com>
2025-06-24 09:43:54 +0530

    kaise ho bhai? using SSH

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("kaise ho bhai? !");
+  res.send("kaise ho bhai? using SSH !");
 });
 
 app.listen(port, () => {
